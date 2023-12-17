d89823f735ad8e37d09ea5736a6752ca5a98ff44
Alexey Bylina <imimmortallyingwork@yandex.ru>
2023-12-17 15:55:43 +0700

    axios - dead access token interceptor added

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosRequestConfig } from "axios";
+import { AuthResponse } from "Models/response/AuthResponse";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 export const API_URL = 'http://localhost:5000/api';
 
@@ -10,6 +11,25 @@ const $api = axios.create({
 $api.interceptors.request.use((config)=>{
     config.headers.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
     return config;
+})
+
+$api.interceptors.response.use((config) => {
+    return config;
+}, async (error)=>{
+    const originalRequest = error.config;
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+        // флаг при первой отправке ставлю тру, чтобы не запустить бесконечный цикл в случае просроченого и аксес и рефреш токенов
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials:true});
+            localStorage.setItem('accessToken', response.data.accessToken);
+            return $api.request(originalRequest)
+        } catch (e) {
+            console.log(e, 'не авторизован')
+        }
+        // не совсем понимаю как тут пробрасывается ошибка на верхний уровень, но это для случая, когда ошибка != 401
+        throw error;
+    }
 })
 
 export default $api;
