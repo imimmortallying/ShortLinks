8948fe8df4492faa20cd41c141d77f1dbbb06ff5
Alexey Bylina <imimmortallyingwork@yandex.ru>
2024-01-09 17:12:56 +0700

    react query devtools added

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,42 +1,43 @@
 
 import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { MainPage, RedirectPage } from "Pages";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from "react-query";
 import { useAppDispatch } from "shared";
+import { MainPage, RedirectPage } from "Pages";
 
 import { req_AppCheckAuth } from "./api/req_AppCheckAuth";
-
 
 const App = () => {
 
     const dispatchAsync = useAppDispatch();
+    
+    const queryClient = new QueryClient();
 
     // проерка авторизации пользователя при загрузке страницы
-        useEffect(()=>{
-            if (localStorage.getItem('accessToken')){
-                // AuthService.checkAuth()
-                dispatchAsync(req_AppCheckAuth())
+    useEffect(() => {
+        if (localStorage.getItem('accessToken')) {
+            // AuthService.checkAuth()
+            dispatchAsync(req_AppCheckAuth())
 
-            }
-        }, []);
+        }
+    }, []);
 
 
 
     return (
-        <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path=":linkAlias" element={<RedirectPage />} />
-        </Routes>
-      </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path=":linkAlias" element={<RedirectPage />} />
+                </Routes>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
 
 
     )
 }
 
 export default App;
-
-function dispatchAsync(arg0: any) {
-    throw new Error("Function not implemented.");
-}
